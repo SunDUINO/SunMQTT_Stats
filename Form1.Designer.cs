98915850b499a264e9fbfcf24fe70153c89049d5
SunRiver <33264278+SunDUINO@users.noreply.github.com>
2020-10-10 23:07:04 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace SunMQTT_Stats
+{
+    partial class logowanie
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.StatusCHK = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BrokerTxtB = new System.Windows.Forms.TextBox();
+            this.PortTxtB = new System.Windows.Forms.TextBox();
+            this.UserTxB = new System.Windows.Forms.TextBox();
+            this.PasswordTxtB = new System.Windows.Forms.TextBox();
+            this.PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.BtnConnect = new System.Windows.Forms.PictureBox();
+            this.dragControl = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StatusCHK)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnConnect)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Green;
+            this.panel1.Controls.Add(this.StatusCHK);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(310, 41);
+            this.panel1.TabIndex = 0;
+            // 
+            // StatusCHK
+            // 
+            this.StatusCHK.Image = global::SunMQTT_Stats.Properties.Resources.wifi_512;
+            this.StatusCHK.Location = new System.Drawing.Point(265, 6);
+            this.StatusCHK.Name = "StatusCHK";
+            this.StatusCHK.Size = new System.Drawing.Size(35, 31);
+            this.StatusCHK.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.StatusCHK.TabIndex = 1;
+            this.StatusCHK.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MQTT Connect";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(46, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Broker :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(60, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Port :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(57, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "User :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(29, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password :";
+            // 
+            // BrokerTxtB
+            // 
+            this.BrokerTxtB.Location = new System.Drawing.Point(105, 76);
+            this.BrokerTxtB.Name = "BrokerTxtB";
+            this.BrokerTxtB.Size = new System.Drawing.Size(176, 20);
+            this.BrokerTxtB.TabIndex = 5;
+            // 
+            // PortTxtB
+            // 
+            this.PortTxtB.Location = new System.Drawing.Point(105, 103);
+            this.PortTxtB.Name = "PortTxtB";
+            this.PortTxtB.Size = new System.Drawing.Size(176, 20);
+            this.PortTxtB.TabIndex = 6;
+            // 
+            // UserTxB
+            // 
+            this.UserTxB.Location = new System.Drawing.Point(105, 130);
+            this.UserTxB.Name = "UserTxB";
+            this.UserTxB.Size = new System.Drawing.Size(176, 20);
+            this.UserTxB.TabIndex = 7;
+            // 
+            // PasswordTxtB
+            // 
+            this.PasswordTxtB.Location = new System.Drawing.Point(105, 157);
+            this.PasswordTxtB.Name = "PasswordTxtB";
+            this.PasswordTxtB.Size = new System.Drawing.Size(176, 20);
+            this.PasswordTxtB.TabIndex = 8;
+            // 
+            // PictureBox2
+            // 
+            this.PictureBox2.Image = global::SunMQTT_Stats.Properties.Resources.footer_logo;
+            this.PictureBox2.Location = new System.Drawing.Point(12, 211);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(100, 71);
+            this.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox2.TabIndex = 9;
+            this.PictureBox2.TabStop = false;
+            this.PictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Image = global::SunMQTT_Stats.Properties.Resources.icon_400_affiliate_whatsgreen3x_300x300;
+            this.BtnConnect.Location = new System.Drawing.Point(223, 211);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(75, 71);
+            this.BtnConnect.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BtnConnect.TabIndex = 0;
+            this.BtnConnect.TabStop = false;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // dragControl
+            // 
+            this.dragControl.Fixed = true;
+            this.dragControl.Horizontal = true;
+            this.dragControl.TargetControl = this.panel1;
+            this.dragControl.Vertical = true;
+            // 
+            // logowanie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(310, 298);
+            this.Controls.Add(this.PictureBox2);
+            this.Controls.Add(this.PasswordTxtB);
+            this.Controls.Add(this.UserTxB);
+            this.Controls.Add(this.PortTxtB);
+            this.Controls.Add(this.BrokerTxtB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnConnect);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "logowanie";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.StatusCHK)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BtnConnect)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox BtnConnect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox BrokerTxtB;
+        private System.Windows.Forms.TextBox PortTxtB;
+        private System.Windows.Forms.TextBox UserTxB;
+        private System.Windows.Forms.TextBox PasswordTxtB;
+        private System.Windows.Forms.PictureBox PictureBox2;
+        private System.Windows.Forms.PictureBox StatusCHK;
+        private Bunifu.Framework.UI.BunifuDragControl dragControl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using MetroFramework;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+
+namespace SunMQTT_Stats
+{
+    public partial class logowanie : Form
+    {
+        public logowanie()
+        {
+            InitializeComponent();
+
+            BrokerTxtB.Text = "192.168.0.178";
+            PortTxtB.Text = "1883";
+            UserTxB.Text = "khadas";
+            PasswordTxtB.Text = "password";
+            StatusCHK.Visible = false;
+
+        }
+
+        static MqttClient client;
+
+      
+
+        #region ---------------> Przyciski
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new MqttClient(BrokerTxtB.Text, int.Parse(PortTxtB.Text), false, MqttSslProtocols.None, null, null);
+                client.ProtocolVersion = MqttProtocolVersion.Version_3_1;
+                byte code = client.Connect(Guid.NewGuid().ToString(), UserTxB.Text, PasswordTxtB.Text);
+                if (code == 0)
+                {
+                    StatusCHK.Visible = true;
+
+                    MetroMessageBox.Show(this, "Successfully Connect to Broker", "Message", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                    client.Subscribe(new string[] { "$SYS/broker/version", "$SYS/broker/uptime", "$SYS/broker/timestamp" }, new byte[] { 0, 0, 0 });
+                }
+
+                else MetroMessageBox.Show(this, "ERROR !! Connection Fail", "Message", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+
+            catch (Exception)
+            {
+                MetroMessageBox.Show(this, "Wrong Messages Format", "Message", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+        }
+
+        #endregion
+
+
+       
+    }
+}
